739824c10119ce86f8679c3860315e0c1e4d9b95
Luiz Alves <luizaaf5434@gmail.com>
2022-09-30 09:13:48 -0300

    Script de captura 02

--- run_server.sh
+++ run_server.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-mkdir /tmp/quic-data
-cd /tmp/quic-data
-wget -p --save-headers https://www.example.org &> /dev/null
-
 cd ~/chromium/src
 
 ./out/Default/quic_server \
---quic_response_cache_dir=/tmp/quic-data/www.example.org \
+--quic_response_cache_dir=/home/ubuntu/html \
 --certificate_file=net/tools/quic/certs/out/leaf_cert.pem \
 --key_file=net/tools/quic/certs/out/leaf_cert.pkcs8 &
